afed5ad3a3c085b36aa73864187d81cee19701f3
Chris <christianmmurga@gmail.com>
2015-08-28 01:57:13 -0300

    Primeros tests

--- main.c
+++ main.c
@@ -3,5 +3,19 @@
 
 int main (void){
 	// COMPLETAR AQUI EL CODIGO
+
+	printf( "la longitud de ’hola’ es = %d \n", palabraLongitud( "hola" ) );
+
+
+	if( palabraMenor( "merced", "mercurio" ) )
+		printf( "TRUE\n" ); else printf( "FALSE\n" );
+
+
+
+	nodo *miNodo = nodoCrear( palabraCopiar( "algunaPalabra" ) );
+
+	nodoBorrar( miNodo );	
+	
+
 	return 0;
 }
